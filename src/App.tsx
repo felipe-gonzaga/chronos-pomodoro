b27b8cd02bc4cdaabcfcd5ec8c96c43ed2b855d4
Felipe Gonzaga <gonzaga.lipe@gmail.com>
2026-05-06 22:30:07 -0300

    Criacao de componente footer; Alteracao do thema dark e ligth

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -53,9 +54,12 @@ export function App() {
 
           <div className='formRow'>
             <DefaultButton icon={<PlayCircleIcon />} />
-            <DefaultButton color='red' icon={<StopCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer></Footer>
       </Container>
     </>
   );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,63 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import style from './styles.module.css';
+import React, { useEffect, useState } from 'react';
 
 export function Menu() {
+  type AvailableTheme = 'dark' | 'light';
+  const [theme, setTheme] = useState<AvailableTheme>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); //Não segue o link
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('este componente sera atualizado');
+    };
+  }, [theme]);
+
   return (
     <nav className={style.menu}>
-      <a className={style.menuLink} href='#'>
+      <a
+        className={style.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
         <HouseIcon />
       </a>
-      <a className={style.menuLink} href='#'>
+      <a
+        className={style.menuLink}
+        href='#'
+        aria-label='Ver Histórico'
+        title='Ver Histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={style.menuLink} href='#'>
+      <a
+        className={style.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={style.menuLink} href='#'>
+      <a
+        className={style.menuLink}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
