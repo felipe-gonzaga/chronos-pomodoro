513ed6813107a391a80bbe38dbe7fb13a0493b20
Felipe Gonzaga <gonzaga.lipe@gmail.com>
2026-05-08 23:31:20 -0300

    logica para funcionamento dos ciclos utilizando contexto

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,19 +1,36 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 import style from './styles.module.css';
 
 export function Cycles() {
+  const { state } = useTaskContext();
+
+  const cycleStep = Array.from({ length: state.currentCycle });
+  const cycleDescriptionMap = {
+    workTime: 'foco',
+    shortBreakTime: 'descanso curto',
+    longBreakTime: 'desconso longo',
+  };
+
   return (
     <>
       <div className={style.cycles}>
         <span>Ciclos:</span>
         <div className={style.cycleDots}>
-          <span className={`${style.cycleDot} ${style.workTime}`}></span>
-          <span className={`${style.cycleDot} ${style.shortBreakTime}`}></span>
-          <span className={`${style.cycleDot} ${style.workTime}`}></span>
-          <span className={`${style.cycleDot} ${style.shortBreakTime}`}></span>
-          <span className={`${style.cycleDot} ${style.workTime}`}></span>
-          <span className={`${style.cycleDot} ${style.shortBreakTime}`}></span>
-          <span className={`${style.cycleDot} ${style.workTime}`}></span>
-          <span className={`${style.cycleDot} ${style.longBreakTime}`}></span>
+          {cycleStep.map((_, index) => {
+            const nextCycle = getNextCycle(index);
+            const nextCycleType = getNextCycleType(nextCycle);
+
+            return (
+              <span
+                key={`${nextCycleType}_${nextCycle}`}
+                className={`${style.cycleDot} ${style[nextCycleType]}`}
+                aria-label={`Indicador de ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+                title={`Indicador de ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+              ></span>
+            );
+          })}
         </div>
       </div>
     </>
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,17 +1,72 @@
-import { PlayCircleIcon } from 'lucide-react';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
-  //const [taskNName, setTaskName] = useState('');
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log('DEU CERTO', taskNameInput.current?.value);
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining: secondsRemaining,
+        formattedSecoundsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handleInterruptTask() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: null,
+        secoundRemaining: 0,
+        formattedSecoundsRemaining: '00:00',
+        tasks: prevState.tasks.map(task => {
+          if (prevState.activeTask && prevState.activeTask.id == task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
+      };
+    });
   }
 
   return (
@@ -23,6 +78,7 @@ export function MainForm() {
           labelText='Task'
           placeholder='Digite algo'
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
@@ -30,12 +86,33 @@ export function MainForm() {
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
 
-      <div className='formRow'>
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className='formRow'>
+          <Cycles />
+        </div>
+      )}
 
       <div className='formRow'>
-        <DefaultButton icon={<PlayCircleIcon />} />
+        {!state.activeTask && (
+          <DefaultButton
+            type='submit'
+            icon={<PlayCircleIcon />}
+            aria-label='Iniciar nova tarefa'
+            title='Iniciar nova tarefa'
+            key='botaoSubmit'
+          />
+        )}
+        {!!state.activeTask && (
+          <DefaultButton
+            type='button'
+            icon={<StopCircleIcon />}
+            color='red'
+            aria-label='Interromper nova tarefa'
+            title='Interromper nova tarefa'
+            onClick={handleInterruptTask}
+            key='botaoButton'
+          />
+        )}
       </div>
     </form>
   );
