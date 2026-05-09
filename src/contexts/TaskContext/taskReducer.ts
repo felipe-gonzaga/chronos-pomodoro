fc6a50f7595d5e4b4c06eef97994e5bc959d225a
Felipe Gonzaga <gonzaga.lipe@gmail.com>
2026-05-09 00:31:13 -0300

    migrando o codigo para usar reducer

---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,43 @@
+import { act } from 'react';
+import type { TaskStateModel } from '../../models/TaskStateModel';
+import { TaskActionTypes, type TaskActionModel } from './taskActions';
+import { getNextCycle } from '../../utils/getNextCycle';
+
+export function taskReducer(
+  state: TaskStateModel,
+  action: TaskActionModel,
+): TaskStateModel {
+  switch (action.type) {
+    case TaskActionTypes.START_TASK: {
+      const newTask = action.payload;
+      const nextCycle = getNextCycle(state.currentCycle);
+      const secondsRemaining = newTask.duration * 60;
+      return {
+        ...state,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secoundRemaining: secondsRemaining,
+        tasks: [...state.tasks, newTask],
+      };
+    }
+    case TaskActionTypes.INTERRUPT_TASK: {
+      return {
+        ...state,
+        activeTask: null,
+        secoundRemaining: 0,
+        formattedSecoundsRemaining: '00:00',
+        tasks: state.tasks.map(task => {
+          if (state.activeTask && state.activeTask.id == task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
+      };
+    }
+    case TaskActionTypes.RESET_TASK: {
+      return state;
+    }
+  }
+
+  return state;
+}
