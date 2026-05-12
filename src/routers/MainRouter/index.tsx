404bd3095724976c2ce38cd37be0f7b48f6b55e4
Felipe Gonzaga <gonzaga.lipe@gmail.com>
2026-05-11 21:59:16 -0300

    criacao componente para routers e scroll top

---
+++ src/routers/MainRouter/index.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+import { AboutPomodoro } from '../../pages/AboutPomodoro';
+import { Home } from '../../pages/Home';
+import { NotFound } from '../../pages/NotFound';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scroll({ top: 0 });
+  }, [pathname]);
+
+  return null;
+}
+
+export function MainRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about-pomodoro' element={<AboutPomodoro />}></Route>
+
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
+  );
+}
